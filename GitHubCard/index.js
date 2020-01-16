3751ae5f4b32c84bb75b35e75cfb249d71802aec
Devin Dias <devindias.tech@gmail.com>
2020-01-16 07:41:49 -0600

    adds github cards too html
    cleans up dead code
    creats cards in js
    loops over friends to call to api
    set api response in cards

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,55 +1,82 @@
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
+var friends = [
+  'tetondan',
+  'dustinmyers',
+  'justsml',
+  'luishrd',
+  'bigknell'
+]
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
+container = document.querySelector('.cards')
 
-   Skip to Step 3.
-*/
+friends.forEach(item => {
+  axios.get('https://api.github.com/users/' + item)
+    .then( response => {
+      console.log(response)
+        container.appendChild(createCard(response))
+    })
+    .catch( error => {
+        console.log("Error:", error);
+    }) 
+})
 
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
+ 
 
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
+  function createCard(object){
 
-const followersArray = [];
+    a = document.createElement('div');
+    b = document.createElement('img');
+    c = document.createElement('div');
+    d = document.createElement('h3');
+    e  = document.createElement('p');
+    f  = document.createElement('p');
+    g = document.createElement('p');
+    h = document.createElement('a');
+    i = document.createElement('p');
+    j = document.createElement('p');
+    k = document.createElement('p');
+  
+  
+    a.appendChild(b);
+    a.appendChild(c);
+    c.appendChild(d);
+    c.appendChild(e);
+    c.appendChild(f);
+    c.appendChild(g);
+ 
+    c.appendChild(i)
+    c.appendChild(j)
+    c.appendChild(k)
+    
+    a.classList.add('card')
+    c.classList.add('card-info')
+    d.classList.add('name')
+    e.classList.add('username')
+  
+    b.setAttribute('src', object.data.avatar_url);
+    d.textContent = object.data.name;
+    e.textContent = object.data.login;
+    f.textContent = object.data.location;
+    g.textContent = "Profile: ";
+    h.href = object.data.html_url;
+    var text = document.createTextNode(object.data.url)
+    h.appendChild(text)
+    g.appendChild(h);
+    i.textContent = 'Followers: ' + object.data.followers;
+    j.textContent = 'Following: ' + object.data.following;
+    k.textContent = 'Bio: ' + object.data.bio;
+  
+  
+    console.log(a)
+    return a
+  
+  }
+  
+  
+  
+ 
+  
+  
+  
 
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
-
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
-
-*/
-
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+    
+  
